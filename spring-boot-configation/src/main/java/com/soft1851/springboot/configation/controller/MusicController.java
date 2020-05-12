06b0dc32b0f5a88336a7edba3f90499157b44b49
Kuzma77 <2673327266@qq.com>
2020-05-12 17:15:15 +0800

    再学SpringBoot

---
+++ spring-boot-configation/src/main/java/com/soft1851/springboot/configation/controller/MusicController.java
@@ -0,0 +1,33 @@
+package com.soft1851.springboot.configation.controller;
+
+
+import com.soft1851.springboot.configation.model.Music;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author: mq_xu
+ * @date: 2020/5/12
+ * @description:
+ */
+@RestController
+@Slf4j
+public class MusicController {
+    @Resource
+    private Music music;
+
+    @Value("${music.name}")
+    private String name;
+
+    @GetMapping("/music")
+    public Music music() {
+        log.info(String.valueOf(music));
+        log.info(name);
+        return music;
+    }
+
+}
